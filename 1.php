6e0cec9f17b4f098750c4fda90a64c46a9d09d1d
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-17 16:13:45 +0530

    String Functions Practiced

---
+++ 1.php
@@ -0,0 +1,42 @@
+<?php
+    
+   //echo strlen("Hello");
+
+   //echo str_replace("World","Nischay","Hello World");
+
+   //echo strpos("Hello World","W");
+   
+   //echo substr("Hello World",3,8);
+   
+   //echo strtolower("HELLO WORLD");
+
+   //echo strtoupper("hello world");
+   
+   //echo trim("Hello World","H d");
+   
+   //$arr = array("Hello","World");
+   //echo implode(" ",$arr);
+   
+   //print_r(explode(" ","Hello World"));
+
+   //echo htmlspecialchars("&a <b> © ");
+   
+   //echo htmlentities("&a <b> © ");
+
+   //echo str_repeat("Hello World </br>",5);
+
+   //echo strrev("Hello World");
+
+   //echo str_shuffle("Hello World");
+
+   //print_r(str_split("HelloWorld",2));
+
+   //echo str_word_count("Hello World");
+
+   //echo substr_replace("Hello World","Duniya",6);
+
+   
+
+
+
+?>
