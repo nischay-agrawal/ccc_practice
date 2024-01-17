93fe64c24d5ebbe4eaebe12f10d7c311ca3c1869
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-17 17:21:12 +0530

    Edit

--- 1.php
+++ 1.php
@@ -33,8 +33,18 @@
 
    //echo str_word_count("Hello World");
 
-   //echo substr_replace("Hello World","Duniya",6);
+   //echo substr_replace("Hello World","Duniya",6); 
 
+   //echo str_pad("Hello World",20,".",STR_PAD_LEFT);
+
+   //echo strcoll("Hello World","Hello World");
+
+   //echo strcspn("Hello world!","w",0,8); // The start position is 0 and the length of the search string is 8.
+
+   //echo stristr("Hello world!","WORLD",false);
+
+   //echo ucfirst("hello world");
+
+   //echo ucwords("hello world");
    
-
 ?>
