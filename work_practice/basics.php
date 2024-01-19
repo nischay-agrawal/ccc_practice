ab2551ef98e4914f9b9b98c751578cda9cd0206b
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-19 11:16:52 +0530

    Edits

--- work_practice/basics.php
+++ work_practice/basics.php
@@ -69,7 +69,72 @@
     //echo $num2--; //5
     //echo --$num2; //4
 
-    
+    // if ($num1>0) {
+    //     echo "Positive";
+    // }
+
+    // if ($num1>0) {
+    //     echo "Positive";
+    // } else {
+    //     echo "Negative";
+    // }
+
+    // if ($num1 > 0) {
+    //     echo "Positive";
+    // } elseif ($num1 < 0) {
+    //     echo "Negative";
+    // } else {
+    //     echo "Zero";
+    // }
+
+    // if ($num1>0) {
+    //     if ($num2>0) {
+    //         echo "Both Positive";
+    //     } else {
+    //         echo "Num 2 Negative";
+    //     }
+    // } else {
+    //     echo "Num 1 Negative";
+    // }
+
+    // $dayOfWeek = "Monday";
+    // switch ($dayOfWeek) {
+    //     case "Monday":
+    //         echo "It's the start of the week!";
+    //         break;
+    //     case "Tuesday":
+    //     case "Wednesday":
+    //     case "Thursday":
+    //         echo "It's a weekday.";
+    //         break;
+
+    //     case "Friday":
+    //         echo "TGIF! It's Friday!";
+    //         break;
+
+    //     case "Saturday":
+    //     case "Sunday":
+    //         echo "It's the weekend!";
+    //         break;
+
+    //     default:
+    //         echo "Invalid day of the week.";
+    // }
+
+    for ($i = 1; $i <= 5; $i++) {
+        echo $i . " ";
+    }
+
+    $i = 1;
+        while ($i <= 5) {
+            echo $i . " ";
+            $i++;
+        }
+
+    $colors = array("Red", "Green", "Blue");
+    foreach ($colors as $color) {
+        echo $color . " ";
+    }
 
 
 ?>
