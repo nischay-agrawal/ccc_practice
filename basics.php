a00180c96cd36e35dd9eb20e3c7794014727e1ec
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-19 10:16:01 +0530

    New and Edits

---
+++ basics.php
@@ -0,0 +1,60 @@
+<?php
+    
+    // var_dump(5);
+    // var_dump("Nischay");
+    // var_dump(3.14);
+    // var_dump(true);
+    // var_dump([1, 2, 3]);
+    // var_dump(NULL);
+
+    // echo abs(-12.5);
+    // echo ceil(87.9); //round number up
+    // echo floor(87.9); // round number down
+    // echo round(4.129097,2);
+
+    // echo pow(5,3);
+    // echo sqrt(25);
+
+    // echo rand(99,999);
+
+    //echo number_format("10000","2",".",",");
+
+    
+    $num1 = 10;
+    $num2 = 5;
+    
+    //Arithmetic Operators  
+    //echo $num1 + $num2 ;
+    //echo $num1 - $num2 ;
+    //echo $num1 * $num2 ;
+    //echo $num1 / $num2 ;
+    //echo $num1 % $num2 ;
+    //echo `$num1  $num2`;
+
+    // Assignment Operators  
+    // $num1=$num2 ;
+    // echo "$num1 $num2";
+    // $num1+=$num2 ;
+    // echo "$num1 $num2";
+    //$num1-=$num2 ;
+    //echo "$num1 $num2";
+    // $num1*=$num2 ;
+    // echo "$num1 $num2";
+    // $num1/=$num2 ;
+    // echo "$num1 $num2";
+    // $num1%=$num2 ;
+    // echo "$num1 $num2";
+
+    // Comparision Operators
+    // var_dump($num1==$num2);
+    // var_dump($num1===$num2);
+    // var_dump($num!=$num2);
+    // var_dump($num1!==$num2);
+    // var_dump($num1>$num2);
+    // var_dump($num1<$num2);
+    // var_dump($num1>=$num2);
+    // var_dump($num1<=$num2);
+
+    //Logical Operators
+    echo $num1 && $num2 ;
+?>
