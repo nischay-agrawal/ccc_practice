40debe652d4857ae8255e103f27440adf62a3271
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-19 10:46:49 +0530

    Edits

--- String_Exercise/string_exercise_2.php
+++ String_Exercise/string_exercise_2.php
@@ -7,5 +7,5 @@
     echo str_replace($text,"Fine, thank you!",$text); // Replacing text to Fine, Thank You!
     echo substr($text,0,10); // Extracting first 10
     echo substr($text,8); // From 8th char to end
-    
+
 ?>
