8ee5b209661cf886efd7fab203bde627cba5f23b
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-30 10:12:38 +0530

    Destruct

--- work_practice/magic_methods_class.php
+++ work_practice/magic_methods_class.php
@@ -2,12 +2,17 @@
     class Person {
         public $name;
     
-        function __construct($name) {
-            $this->name = $name;
-            echo "{$this->name} has been created.\n";
+        // function __construct($name) {
+        //     $this->name = $name;
+        //     echo "{$this->name} has been created.\n";
+        // }
+
+        function __destruct() {
+            echo "Person object is being destroyed.\n";
         }
     }
     
     $person = new Person("Nischay");
+    unset($person);
     
 ?>
