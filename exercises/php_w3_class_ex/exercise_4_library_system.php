b8f13d441b1db6292bc7ec0b4f8d7a5d43f85d5c
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-30 12:31:28 +0530

    Exercise

---
+++ exercises/php_w3_class_ex/exercise_4_library_system.php
@@ -0,0 +1,41 @@
+<?php
+    class Book {
+        public $title;
+        public $author;
+
+        public function displayInfo() {
+            echo "Title: $this->title, Author: $this->author";
+        }
+    }
+
+    class Library {
+        private $books = [];
+
+        public function addBook(Book $book) {
+            $this->books[] = $book;
+        }
+
+        public function displayAllBooks() {
+            foreach ($this->books as $book) {
+                $book->displayInfo();
+                echo "<br>";
+            }
+        }
+    }
+
+    $book1 = new Book();
+    $book1->title = "The Catcher in the Rye";
+    $book1->author = "J.D. Salinger";
+    //$book1->displayInfo();
+
+    $book2 = new Book();
+    $book2->title = "To Kill a Mockingbird";
+    $book2->author = "Harper Lee";
+
+    $library = new Library();
+    $library->addBook($book1);
+    $library->addBook($book2);
+
+    $library->displayAllBooks();
+
+?>
