3488601e381fdc47ac9bb697adf4dfc593006387
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-30 11:32:52 +0530

    Exercise

---
+++ exercises/php_w3_class_ex/exercise_1_simple_calculator.php
@@ -0,0 +1,28 @@
+<?php
+    class Calculator {
+        public function add($a, $b) {
+            return $a + $b;
+        }
+
+        public function subtract($a, $b) {
+            return $a - $b;
+        }
+
+        public function multiply($a, $b) {
+            return $a * $b;
+        }
+
+        public function divide($a, $b) {
+            if ($b != 0) {
+                return $a / $b;
+            } else {
+                return "Cannot divide by zero";
+            }
+        }
+    }
+
+    // Example Usage:
+    $calculator = new Calculator();
+    echo $calculator->add(5, 3); // Output: 8
+
+?>
