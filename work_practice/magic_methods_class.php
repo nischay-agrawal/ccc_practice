d195b25ece2e89ab5094d16740f4005cf23ece10
Nischay Agrawal <nischayagrawal123@gmail.com>
2024-01-30 10:19:06 +0530

    get

--- work_practice/magic_methods_class.php
+++ work_practice/magic_methods_class.php
@@ -1,18 +1,30 @@
 <?php
     class Person {
-        public $name;
+        
+        // public $name;
     
         // function __construct($name) {
         //     $this->name = $name;
         //     echo "{$this->name} has been created.\n";
         // }
 
-        function __destruct() {
-            echo "Person object is being destroyed.\n";
+        // function __destruct() {
+        //     echo "Person object is being destroyed.\n";
+        // }
+
+        private $data = ['name' => 'Nischay', 'age' => 20];
+
+        //for reading data fron private or protected
+        public function __get($name) {  
+            return $this->data[$name];
         }
     }
     
-    $person = new Person("Nischay");
-    unset($person);
+    // $person = new Person("Nischay");
+    // unset($person);
+
+    $person = new Person();
+    echo $person->name . "\n";
+    echo $person->age . "\n";
     
 ?>
